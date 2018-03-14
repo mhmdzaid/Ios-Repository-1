8ff5ea2c5a5d1944df9317146ee2ef7bf13f99b6
mohamed zead <sayedabdo@Sayeds-Mac.local>
2018-03-14 10:30:08 -0700

    schedule frame and expandable tableView

--- AMS project/AMS/controller/scheduleVC.swift
+++ AMS project/AMS/controller/scheduleVC.swift
@@ -23,7 +23,10 @@ class scheduleVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
                                                                                                                         ]
     
     let days  = ["  saturday","  sunday","  monday","  tuseday","  wednisday ","  thursday"]
-   //outlets
+    let minus =  UIImage(named: "Shape 1")
+    let plus = UIImage(named: "pls")
+    //outlets
+    @IBOutlet weak var profImageInfo: UIImageView!
     @IBOutlet weak var professorImage: UIImageView!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var infoView: UIView!
@@ -38,18 +41,27 @@ class scheduleVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
     
     @IBAction func okPressed(_ sender: Any) {
         infoView.isHidden = true
+       tableView.alpha = 1
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
        tableView.delegate = self
         tableView.dataSource = self
         infoView.isHidden = true
+        profImageInfo.layer.cornerRadius = 25
+        profImageInfo.clipsToBounds = true
+        profImageInfo.backgroundColor = #colorLiteral(red: 0.1379489751, green: 0.6505600847, blue: 1, alpha: 1)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier:"cell" ) as? scheduleCellTableViewCell else{
             return UITableViewCell()
         }
+        cell.ProfImage.layer.cornerRadius = 33
+        cell.ProfImage.clipsToBounds = true
+        cell.ProfImage.backgroundColor = UIColor.white
         cell.timeLbl.text = subject[indexPath.row].subjects["time"]
         cell.subjectLbl.text = subject[indexPath.row].subjects["subject"]
         cell.TypeLbl.text = subject[indexPath.row].subjects["type"]
@@ -73,7 +85,7 @@ class scheduleVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
      
         subjectInfo.text = subject[indexPath.row].subjects["subject"]
         startTime.text = subject[indexPath.row].subjects["time"]
-        
+         tableView.alpha = 0.4
     }
     
     
@@ -81,47 +93,41 @@ class scheduleVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
         let button = UIButton(type: .system)
-        button.setTitle(days[section], for: .		normal)
+      // button.imageRect(forContentRect: CGRect(x: 30, y: 20, width:10, height:10))
+        
+        button.setTitle(days[section], for: .normal)
         button.setTitleColor(#colorLiteral(red: 0.1379489751, green: 0.6505600847, blue: 1, alpha: 1), for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
         button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         button.titleLabel?.textAlignment = NSTextAlignment.left
-       let imageViewIn = UIImageView(frame: CGRect(x: 32, y: 35, width:30, height: 10))
-        let signImage =  UIImage(named: "Shape 1")
-        imageViewIn.backgroundColor = UIColor.clear
-       imageViewIn.image = signImage
-       button.addSubview(imageViewIn)
-       
-       
-        
-        
+        button.imageView?.contentMode = .scaleAspectFit
         button.addTarget(self, action: #selector(expandCollapse(button:)), for: .touchUpInside)
+        button.contentHorizontalAlignment = .left
         button.tag = section
-        
-      
-        
-        
-              return button
+        button.setImage(minus, for: .normal)
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.black.cgColor
+        if subject[section].isExpanded{
+            button.setImage(minus, for: .normal)
+            button.imageView?.image = minus
+        }else{
+            
+            button.setImage(plus, for: .normal)
+            
+        }
+        return button
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 70
-    }
+        return 100    }
     
     @objc func expandCollapse(button:UIButton){
         
         print(button.tag)
         let section = button.tag
         var indexPaths = [IndexPath]()
-         let signImage =  UIImage(named: "Shape 1")
-         let signImage2 =  UIImage(named: "add")
-         let imageViewIn = UIImageView(frame: CGRect(x: 32, y: 35, width:30, height: 10))
-        if imageViewIn.image == signImage{
-            imageViewIn.image = signImage2
-        }
-        else{imageViewIn.image = signImage}
        for row in 0..<subject[section].subjects.count
        {
             let indexpath = IndexPath(row: row, section: section)
@@ -132,9 +138,15 @@ class scheduleVC: UIViewController ,UITableViewDelegate, UITableViewDataSource{
         subject[section].isExpanded = !isExpanded
         if isExpanded{
             tableView.deleteRows(at: indexPaths, with: UITableViewRowAnimation.fade)
+             button.setImage(plus, for: .normal)
+           
         }else{
+            
             tableView.insertRows(at: indexPaths, with: UITableViewRowAnimation.top)
+             button.setImage(minus, for: .normal)
+        
         }
+     
         
         
     }
