b8a28dd539a854b07c754f8dcaa81ba8e7e012e3
mohamed zead <sayedabdo@Sayeds-Mac.local>
2018-04-19 04:18:39 -0700

    last commit

--- AMS project/AMS/controller/ManualAttendanceVC.swift
+++ AMS project/AMS/controller/ManualAttendanceVC.swift
@@ -8,25 +8,25 @@
 
 import UIKit
 
-class ManualAttendanceVC: UIViewController ,UITableViewDelegate,UITableViewDataSource {
-
+class ManualAttendanceVC: UIViewController ,UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate{
+  
+    var students = ["ahmed ","abdullah " , "gemy ", "diasty"]
+    var studentsUpdated :[String]?
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        searchBar.layer.borderWidth  =  1.5
-        searchBar.layer.borderColor = UIColor.black.cgColor
-        searchBar.layer.cornerRadius = 13
-        searchBar.clipsToBounds = true
-        
+        searchBar.delegate = self
+        studentsUpdated = students
     }
-    @IBOutlet weak var searchBar: UITextField!
+
+    @IBOutlet weak var searchBar: UISearchBar!
     
 
     @IBOutlet weak var tableView: UITableView!
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return studentsUpdated!.count
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -35,9 +35,11 @@ class ManualAttendanceVC: UIViewController ,UITableViewDelegate,UITableViewDataS
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell") as? StudentCell else{
             return UITableViewCell()
         }
-     cell.studentName.text = "ahmed"
-     cell.studentImage.layer.cornerRadius = 39
-     cell.studentImage.clipsToBounds = true
+     cell.layer.borderWidth = 0.6
+     cell.layer.borderColor = UIColor.black.cgColor
+     cell.studentName.text = studentsUpdated![indexPath.row]
+     cell.studentImage.layer.cornerRadius = 35
+     
 
     return cell
     }
@@ -45,5 +47,25 @@ class ManualAttendanceVC: UIViewController ,UITableViewDelegate,UITableViewDataS
         return 100
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            studentsUpdated = students
+            tableView.reloadData()
+            return
+        }
+        studentsUpdated = students.filter { (student) -> Bool in
+        guard let text = searchBar.text?.lowercased() else{return false}
+        return student.lowercased().contains(text)
+        }
+        tableView.reloadData()
+    }
+  
     
-}
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+    }
+    
+    }
+    
+    
+
