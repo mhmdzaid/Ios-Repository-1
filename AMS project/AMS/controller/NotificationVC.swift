9d00b3a18a93a9ac37950ee68f26fa1d1be676eb
mohamed zead <sayedabdo@Sayeds-Mac.local>
2018-06-30 07:00:08 -0700

    adding some constraints to notification cell

--- AMS project/AMS/controller/NotificationVC.swift
+++ AMS project/AMS/controller/NotificationVC.swift
@@ -16,7 +16,7 @@ class NotificationVC: UIViewController , UITableViewDelegate , UITableViewDataSo
         super.viewDidLoad()
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         tableView.delegate = self
-        
+        tableView.dataSource = self 
     }
     
     @IBAction func menuBtnPressed(_ sender: Any) {
@@ -30,20 +30,26 @@ class NotificationVC: UIViewController , UITableViewDelegate , UITableViewDataSo
         }
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "notificationCell") as? NotificationCell
         else{
             return UITableViewCell()
         }
-        
+        cell.layer.borderWidth = 1.3
+        cell.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
         return cell
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 88
-    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
